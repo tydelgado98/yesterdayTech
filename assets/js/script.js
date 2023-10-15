b897e1e2ccdc2f407e11af18320a972db9327c60
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-15 13:55:49 -0400

    making a comment of all the things i can do with this api

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,6 @@
 // br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th 
 // tr tw ua us ve za. Note: you can't mix this param with the sources param.
 
-GET https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=ef1373e5392446349319b79d71c36c49
+// GET https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=ef1373e5392446349319b79d71c36c49
+// that is the api call for the bbc news and its using my actual api key
 
