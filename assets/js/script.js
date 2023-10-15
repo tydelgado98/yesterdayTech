659d24fadca33d5b8cb8b289e93e25856d727644
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-15 13:53:35 -0400

    making a comment of all the things i can do with this api

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+//  The category you want to get headlines for. Possible options: business entertainment generalhealth science sports technology. Note: you can't mix this param with the sources param.
+
+// The 2-letter ISO 3166-1 code of the country you want to get headlines for. Possible options: ae ar at au be bg 
+// br ca ch cn co cu cz de eg fr gb gr hk hu idieilinitjpkrltlvmamxmyngnlnonzphplptrorsrusasesgsiskthtrtwuausveza. Note: you can't mix this param with the sources param.
+
