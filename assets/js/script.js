63d69b7ee0a5c964f68486a80e409cc3db718faa
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-15 22:25:39 -0400

    made everything prepared for tommorow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 
-//  The category you want to get headlines for. Possible options: business entertainment generalhealth science sports technology. Note: you can't mix this param with the sources param.
+//  The category you want to get headlines for. Possible options: business entertainment
+// generalhealth science sports technology. Note: you can't mix this param with the sources param.
 
 // The 2-letter ISO 3166-1 code of the country you want to get headlines for. Possible options: ae ar at au be bg 
-// br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th 
+// br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr 
+// lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th 
 // tr tw ua us ve za. Note: you can't mix this param with the sources param.
 
 // GET https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=ef1373e5392446349319b79d71c36c49
