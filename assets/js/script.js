e24271839ec2bfdaa5506280cbe357c78b953107
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-20 19:20:04 -0400

    made the search section

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ let getCategoryWeather = function (category) {
             console.log(res);
             res.json().then(function (data) {
                 console.log(data);
-                categorySearch.innerHTML = "";
+                categorySearch.innerHTML = `<h2>${category}</h2>`;
 
 
 
