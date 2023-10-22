fdc32c7e31d5d24b95927447bd5453f7690870c7
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-22 15:05:30 -0400

    making the data to transfer

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,29 +1,25 @@
 let newEl = document.getElementById("newsSection");
 let articleContainer = document.getElementById("article-container");
 
-// Parse the URL parameter to get the article data
-const urlParams = new URLSearchParams(window.location.search);
-const articleData = urlParams.get("url");
 
-// Check if article data exists
+
+
+const articleData = JSON.parse(decodeURIComponent(new URLSearchParams(window.location.search).get("url")));
+
 if (articleData) {
-  const article = JSON.parse(decodeURIComponent(articleData));
-
-  // Display the article details on the page
   let articleTitle = document.createElement("h2");
-  articleTitle.textContent = article.title;
+  articleTitle.textContent = articleData.title;
 
   let articleDescription = document.createElement("p");
-  articleDescription.textContent = article.description;
+  articleDescription.textContent = articleData.description;
 
   let articleContent = document.createElement("p");
-  articleContent.textContent = article.content;
+  articleContent.textContent = articleData.content;
 
   articleContainer.appendChild(articleTitle);
   articleContainer.appendChild(articleDescription);
   articleContainer.appendChild(articleContent);
 } else {
-  // Handle the case when no article data is provided
   articleContainer.textContent = "No article data found.";
 }
 
