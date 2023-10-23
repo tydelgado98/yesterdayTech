cb0a01fe47c3564b371df7430848347032afe623
Justin Delgado <delgadojustin100@yahoo.com>
2023-10-23 19:51:55 -0400

    making imgs

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,17 @@
 let newEl = document.getElementById("newsSection");
 let articleContainer = document.getElementById("article-container");
-
 let articleData = JSON.parse(decodeURIComponent(new URLSearchParams(window.location.search).get("url")));
+let footer = document.getElementById("foot");
+
+
+
+
 
 if (articleData) {
+
+footer.style.position = "relative";
+
+
 
 console.log(articleData);
 
@@ -12,16 +20,19 @@ console.log(articleData);
   articleAuthor.textContent = articleData.author;
   articleAuthor.setAttribute("class", "mb-1 mt-4");
 
+
   let articleTitle = document.createElement("h2");
   articleTitle.textContent = articleData.title;
   articleTitle.setAttribute("class", "mt-3");
   articleTitle.setAttribute("class", "mb-4 mt-2 border-bottom border-dark rounded");
   articleTitle.style.paddingBottom = "4rem";
 
+
   let articleDescription = document.createElement("h5");
   articleDescription.textContent = articleData.description;
   articleDescription.setAttribute("class", "mb-4")
   articleDescription.style.marginTop = "4.0rem";
+
 
   let articleContent = document.createElement("a");
   articleContent.textContent = articleData.content;
@@ -31,11 +42,17 @@ console.log(articleData);
   articleContent.style.fontSize = "0.8rem";
   articleContent.style.textDecoration = "none";
 
+  let articleImg = document.createElement("img");
+  articleImg.setAttribute("src", articleData.urlToImage);
+  articleImg.style = "width: 60%; display: block; margin: 4rem auto 2rem;";
+  
+
 
   articleContainer.appendChild(articleAuthor);
   articleContainer.appendChild(articleTitle);
   articleContainer.appendChild(articleDescription);
   articleContainer.appendChild(articleContent);
+  articleContainer.appendChild(articleImg);
  
 } else {
   articleContainer.textContent = "No article data found.";
